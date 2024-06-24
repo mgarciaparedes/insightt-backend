ca6703eba7fbc1c7b496c34280935d4eb0eeba5c
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2024-06-24 09:37:35 -0400

    fix: login successfull message and error

--- controllers/login.js
+++ controllers/login.js
@@ -9,33 +9,27 @@ const postLogin = async (req, res = response) => {
     const user = await Login.findOne({ email: email.toLowerCase() });
     if (!user) {
       return res.status(400).json({
-        ok: false,
-        msg: "Incorrect username or password",
+        message: "Incorrect username or password",
       });
     }
-
-    console.log(user);
 
     //Confirmar los passwords
     const validPassword = bcrypt.compareSync(password, user.password);
 
     if (!validPassword) {
       return res.status(400).json({
-        ok: false,
-        msg: "Incorrect username or password.",
+        message: "Incorrect username or password.",
       });
     }
 
     res.json({
-      ok: true,
-      msg: "login",
+      message: "Login successfull",
       email,
     });
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      ok: false,
-      msg: "Please, contact this sites admin",
+      message: "Please, contact this sites admin",
     });
   }
 };
