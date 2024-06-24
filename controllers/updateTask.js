4dc260ac88a1138f34f6b74cf0f45a9556a776f4
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2024-06-24 00:46:52 -0400

    feat: new message property in updateTask api

--- controllers/updateTask.js
+++ controllers/updateTask.js
@@ -1,4 +1,4 @@
-const Task = require('../models/task');
+const Task = require("../models/task");
 
 const updateTask = async (req, res) => {
   const { id } = req.body;
@@ -8,14 +8,17 @@ const updateTask = async (req, res) => {
   delete updateData._id;
 
   try {
-    const updatedTask = await Task.findByIdAndUpdate(id, updateData, { new: true });
+    const updatedTask = await Task.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
     if (!updatedTask) {
-      return res.status(404).json({ message: 'Task not found' });
+      return res.status(404).json({ message: "Task not found" });
     }
-    res.status(200).json(updatedTask);
+    const taskList = await Task.find({});
+    res.status(200).json({ message: "Task updated successfully", taskList });
   } catch (err) {
-    console.error('Error updating task:', err);
-    res.status(500).json({ message: 'Error updating task', error: err });
+    console.error("Error updating task:", err);
+    res.status(500).json({ message: "Error updating task", error: err });
   }
 };
 
